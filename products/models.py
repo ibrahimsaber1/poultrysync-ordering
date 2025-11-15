63a3c1b852717cad9fe5731e06ba4654a44065f1
unknown <ibrahimsaber622@gmail.com>
2025-11-15 17:25:49 +0200

    create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.conf  import settings
 # Create your models here.
+
+class Product(models.Model):
+    company = models.ForeignKey( 'companies.Company',
+                                on_delete= models.CASCADE, related_name='products')
+    
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    
+    created_by = models.ForeignKey( settings.AUTH_USER_MODEL,
+                                    on_delete=models.SET_NULL,
+                                    null=True,
+                                    related_name='created_products') 
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['name']
+        unique_together = ['company', 'name']
+        
+    def __str__(self):
+        return f"{self.name} ({self.company.name})"
