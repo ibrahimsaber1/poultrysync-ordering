a5843ec924e7641f4e26c5880b251350e96558f7
unknown <ibrahimsaber622@gmail.com>
2025-11-15 17:11:35 +0200

    create user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('operator', 'Operator'),
+        ('viewer', 'Viewer'),
+    )
+    
+    company = models.ForeignKey(
+        'companies.Company',
+        on_delete=models.CASCADE,
+        related_name='users'
+    )
+    
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='viewer')
+    
+    class Meta:
+        ordering = ['username']
+    
+    def __str__(self):
+        return f"{self.username} from: {self.company}"
