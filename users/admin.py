f76f1f6ae83f495e8daea998bab936473b74e4be
Ibrahim Saber <ibrahimsaber622@gmail.com>
2025-11-16 01:33:06 +0200

    update : admin panel to let the admins sees only the users of there own company

--- users/admin.py
+++ users/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ['username', 'email', 'company', 'role', 'is_staff']
+    list_display = ['username', 'email', 'company', 'role', 'is_staff', 'is_superuser']
     list_filter = ['role', 'company', 'is_staff', 'is_superuser']
     search_fields = ['username', 'email', 'company__name']
     
@@ -15,3 +16,13 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
         ('Company Info', {'fields': ('company', 'role')}),
     )
+    
+    def get_queryset(self, request):
+        """
+        Superuser sees all users.
+        Staff users (admin/operator) only see users from their company.
+        """
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(company=request.user.company)
